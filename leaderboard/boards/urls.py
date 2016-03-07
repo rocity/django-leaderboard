6b6404bd39280564a634393378a10310d8896661
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-03-07 13:18:23 +0800

    show items in index page

--- leaderboard/boards/urls.py
+++ leaderboard/boards/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<item_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- leaderboard/boards/views.py
+++ leaderboard/boards/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Item
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world!")
+def index(r):
+    items = Item.objects.all()
+    context = {
+        'item_list': items
+    }
+
+    return render(r, 'boards/index.html', context)
+
+
+def detail(r, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    return render(r, 'boards/details.html', {'item': item})
+
+
+def vote(r, item_id):
+    return HttpResponse("Voting on item %s" % item_id)
