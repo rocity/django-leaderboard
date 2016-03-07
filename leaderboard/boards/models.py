42448756077228c6fa16f1cb85b19daf3e2bc745
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-03-07 11:01:44 +0800

    added Item model

--- leaderboard/boards/models.py
+++ leaderboard/boards/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    """
+    Description: Can be a name of a person, brand, object, etc.
+    """
+    item_text = models.CharField(max_length=255)
+    score = models.IntField()
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
