7108d0293e519d44e07c773c32fc8f5c305b385d
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-03-07 14:58:11 +0800

    improve admin view for items

--- leaderboard/boards/admin.py
+++ leaderboard/boards/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Item
 
+class ItemAdmin(admin.ModelAdmin):
+    fields = ['item_text', 'score']
+    list_display = ('item_text', 'score', 'created')
+
 # Register your models here.
-admin.site.register(Item)
+admin.site.register(Item, ItemAdmin)
