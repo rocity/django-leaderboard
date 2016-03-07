006c39fed03c7d24af60e6bf9d04bb442ce2a8e7
Kevin Paul Kalis <hello@kvnpl.xyz>
2016-03-07 11:25:35 +0800

    enabled admin for boards app

--- leaderboard/boards/models.py
+++ leaderboard/boards/models.py
@@ -7,5 +7,8 @@ class Item(models.Model):
     Description: Can be a name of a person, brand, object, etc.
     """
     item_text = models.CharField(max_length=255)
-    score = models.IntField()
+    score = models.IntegerField()
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.item_text
